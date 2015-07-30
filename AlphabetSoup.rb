945838b4807ddd37bf62ee3d5558c5322fd0c4cb
Kim Rowoon <missrowoon@naver.com>
2015-07-30 19:36:41 +0900

    Delete AlphabetSoup.rb

--- AlphabetSoup.rb
+++
@@ -1,9 +0,0 @@
-def AlphabetSoup(str)
-
-  return str.split("").sort().join("")
-         
-end
-   
-# keep this function call here 
-# to see how to enter arguments in Ruby scroll down   
-AlphabetSoup(STDIN.gets)      
